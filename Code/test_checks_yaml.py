7476bf38daf450336f4870ae3a300a926700d9c6
omar123mok <106643874+omar123mok@users.noreply.github.com>
2023-01-25 17:30:03 +0530

    Update test_checks_yaml.py

--- Code/test_checks_yaml.py
+++ Code/test_checks_yaml.py
@@ -9,7 +9,6 @@ invalid_data = [{'firstname': '', 'lastname': '', 'gender': '', 'tags': []}]
 
 
 def test_validate_keys_valid_input():
-    print(valid_data)
     for x in valid_data:
         assert code.validate_keys(x, 1) == "All required keys are present for data at list index 1"
 
@@ -17,7 +16,7 @@ def test_validate_keys_valid_input():
 def test_validate_keys_invalid_input():
     with pytest.raises(AssertionError):
         for x in invalid_data:
-            code.validate_keys(x, 1) == "All required keys are present for data at list index 1"
+            assert code.validate_keys(x, 1) == "All required keys are present for data at list index 1"
 
 def test_validate_values_valid_input():
     for x in valid_data:
