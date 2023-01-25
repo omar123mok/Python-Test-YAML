e5eacb27024db6884923152cd1d14b68204bd166
omar123mok <106643874+omar123mok@users.noreply.github.com>
2023-01-25 17:32:02 +0530

    Update test_checks_yaml.py

--- Code/test_checks_yaml.py
+++ Code/test_checks_yaml.py
@@ -5,7 +5,8 @@ import Code.checks_yaml as code
 valid_data = [{'firstname': 'John', 'lastname': 'Doe', 'gender': 'male', 'tags': ['student', 'programmer']}]
 
 # Test data with invalid input (missing keys and values)
-invalid_data = [{'firstname': '', 'lastname': '', 'gender': '', 'tags': []}]
+invalid_data_1 = [{'firstname': '', 'lastname': '', 'gender': ''}]
+invalid_data_2 = [{'firstname': '', 'lastname': '', 'gender': '', 'tags': []}]
 
 
 def test_validate_keys_valid_input():
@@ -15,7 +16,7 @@ def test_validate_keys_valid_input():
 
 def test_validate_keys_invalid_input():
     with pytest.raises(AssertionError):
-        for x in invalid_data:
+        for x in invalid_data_1:
             assert code.validate_keys(x, 1) == "All required keys are present for data at list index 1"
 
 def test_validate_values_valid_input():
@@ -25,5 +26,5 @@ def test_validate_values_valid_input():
 
 def test_validate_values_invalid_input():
     with pytest.raises(AssertionError):
-        for x in invalid_data:
+        for x in invalid_data_2:
             assert code.validate_values(x, 1) == "All values are present for data at list index 1"
